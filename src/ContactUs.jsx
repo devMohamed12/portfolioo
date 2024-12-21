3ce8c872da58fd83d0c25b930a609e6ecfac6748
devMohamed12 <133253839+devMohamed12@users.noreply.github.com>
2024-12-21 02:09:10 +0200

    Update ContactUs.jsx
    Signed-off-by: devMohamed12 <133253839+devMohamed12@users.noreply.github.com>

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -1,45 +1,40 @@
-import { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import emailjs from "emailjs-com";
 import { motion } from "framer-motion";
 
 const ContactUs = () => {
-
-
   const label = "text-[17px]";
-  const input = "input focus:outline-none  w-full  ";
-  const initMessage = {
+  const input = "input focus:outline-none w-full";
+
+  const initialValues = {
     name: "",
     email: "",
     subject: "",
     message: "",
   };
-  const [message, setMessage] = useState(initMessage);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setMessage({ ...message, [name]: value });
-  };
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Name is required"),
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+    subject: Yup.string().required("Subject is required"),
+    message: Yup.string().required("Message is required"),
+  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
+  const handleSubmit = (values, { resetForm }) => {
     emailjs
-      .sendForm(
-        "service_6bg8wxw",
-        "template_idi3m9d",
-        e.target,
-        "OnFvf0nGGwNmXVnuA"
-      )
+      .send("service_6bg8wxw", "template_idi3m9d", values, "OnFvf0nGGwNmXVnuA")
       .then(
         (result) => {
           console.log("Message sent:", result.text);
+          resetForm();
         },
         (error) => {
           console.log("Error sending message:", error.text);
         }
       );
-
-    setMessage(initMessage);
   };
 
   return (
@@ -54,119 +49,122 @@ const ContactUs = () => {
         Let's Work Together
       </motion.h2>
 
-      <div className="  ">
-        <form
-          className="lg:w-4/6 w-[90%]"
+      <div>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
           onSubmit={handleSubmit}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.3 }}
         >
-          <motion.div
-            className="my-4"
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.4 }}
-          >
-            <label htmlFor="username" className={label}>
-              Your Name:
-            </label>
-            <input
-              type="text"
-              id="username"
-              placeholder="Enter your name"
-              className={`${input}  `}
-              name="name"
-              onChange={handleChange}
-            />
-          </motion.div>
+          {({ isSubmitting }) => (
+            <Form className="lg:w-4/6 w-[90%]">
+              <motion.div
+                className="my-4"
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.4 }}
+              >
+                <label htmlFor="name" className={label}>
+                  Your Name:
+                </label>
+                <Field
+                  type="text"
+                  id="name"
+                  name="name"
+                  className={`${input}`}
+                  placeholder="Enter your name"
+                />
+                <ErrorMessage
+                  name="name"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </motion.div>
 
-          <motion.div
-            className="mb-5"
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.5 }}
-          >
-            <label htmlFor="email" className={label}>
-              Your Email:
-            </label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Enter your email"
-              className= {`${input}  `}
-              name="email"
-              onChange={handleChange}
-            />
-          </motion.div>
+              <motion.div
+                className="mb-5"
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.5 }}
+              >
+                <label htmlFor="email" className={label}>
+                  Your Email:
+                </label>
+                <Field
+                  type="email"
+                  id="email"
+                  name="email"
+                  className={`${input}`}
+                  placeholder="Enter your email"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </motion.div>
 
-          <motion.div
-            className="mb-5"
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.6 }}
-          >
-            <label htmlFor="subject" className={label}>
-              Enter Subject:
-            </label>
-            <input
-              type="text"
-              id="subject"
-              placeholder="Enter your subject"
-              className= {`${input}  `}
-              name="subject"
-              onChange={handleChange}
-            />
-          </motion.div>
+              <motion.div
+                className="mb-5"
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.6 }}
+              >
+                <label htmlFor="subject" className={label}>
+                  Enter Subject:
+                </label>
+                <Field
+                  type="text"
+                  id="subject"
+                  name="subject"
+                  className={`${input}`}
+                  placeholder="Enter your subject"
+                />
+                <ErrorMessage
+                  name="subject"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </motion.div>
 
-          <motion.div
-            className="mb-5"
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.7 }}
-          >
-            <label htmlFor="message" className={label}>
-              Enter Message:
-            </label>
-            <textarea
-              className=
-              {`${input}  w-full resize-none textarea h-32 `}
-              
-              placeholder="Enter your message"
-              id="message"
-              rows={3}
-              onChange={handleChange}
-              name="message"
-            ></textarea>
-          </motion.div>
+              <motion.div
+                className="mb-5"
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.7 }}
+              >
+                <label htmlFor="message" className={label}>
+                  Enter Message:
+                </label>
+                <Field
+                  as="textarea"
+                  id="message"
+                  name="message"
+                  className={`${input} w-full resize-none textarea h-32`}
+                  placeholder="Enter your message"
+                  rows={3}
+                />
+                <ErrorMessage
+                  name="message"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </motion.div>
 
-          <motion.button
-            type="submit"
-            className="cursor-pointer px-3 py-5  block text-lg rounded-xl bg-teal-600 text-white w-24 "
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.8 }}
-          >
-            Submit
-          </motion.button>
-        </form>
-
-        {/* Second part */}
-        <motion.div
-          className="lg:w-2/6 lg:py-10 hidden"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1 }}
-        >
-          <img
-            className="rounded-md"
-            src="https://images.unsplash.com/photo-1523800503107-5bc3ba2a6f81?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Contact"
-          />
-          {/* Additional contact information can be added here */}
-        </motion.div>
+              <motion.button
+                type="submit"
+                disabled={isSubmitting}
+                className="cursor-pointer px-3 py-5 block text-lg rounded-xl bg-teal-600 text-white w-24"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.8 }}
+              >
+                {isSubmitting ? "Sending..." : "Submit"}
+              </motion.button>
+            </Form>
+          )}
+        </Formik>
       </div>
     </>
   );
